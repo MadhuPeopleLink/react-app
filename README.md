8575a424492114c3ad0bcc15a55ea6a867904181
MadhuPeopleLink <madhusudhan@peoplelinkvc.com>
2017-11-20 14:51:10 +0530

    start it from begining

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-app
-Sample react-app
--- reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-import products from './products.js';
-import categories from './categories.js';
-
-const rootReducer = combineReducers({products, categories});
-
-export default rootReducer;
